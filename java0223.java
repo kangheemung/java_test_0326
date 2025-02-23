3f2e5df733e087abc853807d81625ef655ad612b
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-02-23 23:38:32 +0900

    test

--- java0223.java
+++ java0223.java
@@ -5,3 +5,38 @@ public class Main {
     }
 }
 ///＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println(3 * 5);
+        
+    }
+}
+///＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println(6/4 );
+        
+    }
+}
+///＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println(6.0 / 4 );
+        
+    }
+}
+
+//  期待する出力値
+
+// 1.5
+//==============あまり計算
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println(5 % 3 );
+    }
+}
