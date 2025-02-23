7966c77f3d49663d1ee9d7e9106ee9622d268b4e
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-02-24 00:23:13 +0900

    ##

--- java0223.java
+++ java0223.java
@@ -40,3 +40,71 @@ public class Main {
         System.out.println(5 % 3 );
     }
 }
+
+//================================出力Hellow100world
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hellow"+ 100 +"world");
+    }
+}
+
+
+//================================私の名前は、霧島です。
+public class Main {
+    public static void main(String... args) {
+        System.out.println("私の名前は、"+"霧島です。");
+    }
+}
+
+//==========================
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println((4 + 6 )/ 5);
+    }
+}
+//================
+public class Main {
+    public static void main(String... args) {
+        String text = "paiza";
+        System.out.println(text);
+    }
+}
+//================
+
+public class Main {
+    public static void main(String... args) {
+        String text;
+        text="paiza";
+        System.out.println(text);
+    }
+}
+
+//==========================
+import java.util.*;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("こんにちは");
+        
+    }
+}
+
+
+//==========================
+public class Main {
+    public static void main(String... args) {
+        String greeting;
+        // 以下にコードを追加
+       greeting ="こんばんは";
+        System.out.println(greeting);
+    }
+} 
+//============================
+
+public class Main {
+    public static void main(String... args) {
+        String iff;
+        
+    }
+}
