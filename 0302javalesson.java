f5d2ea29c8b981a4521220222893584b1b8dbd4d
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-02 17:23:33 +0900

    add

--- 0302javalesson.java
+++ 0302javalesson.java
@@ -122,3 +122,169 @@ public class Main {
     });
   } 
 }
+
+
+
+//============================= for==================
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("lemon");
+        
+        for (String fruit : fruits){
+            System.out.printLn(fruit);
+        }
+    }
+}
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("lemon");
+        
+        for (String fruit : fruits){
+            String delicious = fruit + "が美味しい";
+            System.out.println(delicious);
+        }
+    }
+}
+
+
+//============================= appleが美味しい
+//=============================orangeが美味しい
+//=============================lemonが美味しいr==================
+
+//===================条件=========================
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+       Integer i=0;
+       if(i == 0){
+           System.out.println("iは0です。");
+       }
+    }
+}
+
+//===================条件=========================
+public class Main {
+    public static void main(String... args) {
+        Integer i = 5;
+
+        if (i == 5) {
+            System.out.println("iは5");
+        }
+    }
+}
+//===================条件=========================
+public class Main {
+    public static void main(String... args) {
+        Integer i = 10;
+
+        if (i == 10) {
+            System.out.println("iは10");
+
+      }
+    }
+}
+
+//===================条件==else=======================
+public class Main {
+    public static void main(String... args) {
+
+        Integer i = 0;
+
+        if (i == 0) {
+            System.out.println("iは0です");
+        }else{
+           System.out.println("iは0ではないです");
+            
+        }
+    }
+}
+
+
+//===================条件==else=======================
+public class Main {
+    public static void main(String... args) {
+        Integer i = 7;
+
+        if (i == 5) {
+            System.out.println("iは5");
+        }else{
+              System.out.println("iは5ではない");
+        }
+    }
+}
+
+//===================条件==else=======================
+public class Main {
+    public static void main(String... args) {
+
+        Integer i = 1;
+
+        if (i == 0) {
+            System.out.println("iは0です");
+        } else if(i == 1){
+                System.out.println("iは1です");
+        }
+          else{
+                System.out.println("iは1でも0でもありません");
+        }
+      }
+}
+
+
+//===================条件==else=======================
+public class Main {
+    public static void main(String... args) {
+        Integer i = 2;
+
+        if (i == 1) {
+            System.out.println("1位です");
+        } else if(i == 2){
+            System.out.println("2位です");
+        }else{
+            System.out.println("最下位です");
+        }
+    }
+}
+
+
+
+
+
+//===================条件==else=======================
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<Integer> scores =new ArrayList<>();
+        scores.add(1);
+        scores.add(2);
+        scores.add(3);
+        scores.add(4);
+        scores.add(5);
+        
+        scores.forEach(score -> {
+            if (score % 2 == 0){
+                System.out.println(score +"は偶数");
+            }else{ 
+                System.out.println(score +"は奇襲");
+                
+            }
+        });
+        
+    }
+}
