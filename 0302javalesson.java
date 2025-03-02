59033c47fb25d4b54cb992138130ef319b7df266
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-02 16:35:34 +0900

    add

--- 0302javalesson.java
+++ 0302javalesson.java
@@ -41,3 +41,84 @@ public class Main {
     });
     }
 }
+
+
+//======================================処理だでの記述
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("lemon");
+
+        fruits.forEach(x-> System.out.println(x));
+
+    }
+}
+//======================================処理だでの記述
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> animals = new ArrayList<>();
+
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("panda");
+     animals.forEach(x-> System.out.println(x));
+    }
+}
+
+
+//======================================処理だでの記述
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("lemon");
+ fruits.forEach(x-> System.out.println(x));
+    }
+}
+//======================================処理だでの記述
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> users = new ArrayList<>();
+
+        users.add("kirishima");
+        users.add("rokumura");
+        users.add("midorikawa");
+        users.forEach(x->System.out.println(x));
+
+    }
+}
+
+//============================= 全部大文字に変える
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> users = new ArrayList<>();
+
+        users.add("kirishima");
+        users.add("rokumura");
+        users.add("midorikawa");
+    users.forEach(user->{
+      String userUpper = user.toUpperCase();
+      System.out.println(userUpper);
+    });
+  } 
+}
