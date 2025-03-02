da160b9725f6fd706913b449906de0e58b1405d3
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-02 16:22:28 +0900

    add

--- 0302javalesson.java
+++ 0302javalesson.java
@@ -6,8 +6,38 @@ public class Main {
         fruits.add("apple");
         fruits.add("orange");
         fruits.add("lemon");
-        
         System.out.println(fruits.get(0));
         System.out.println(fruits.get(0));
+    }
+}
+//======================================
+import java.util.*;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("lemon");
+
+        fruits.forEach((String fruit)->{
+            System.out.println(fruit);
+        });
+    }
+}
+//======================================
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> users = new ArrayList<>();
+
+        users.add("kirishima");
+        users.add("rokumura");
+        users.add("midorikawa");
+    users.forEach((String user)->{
+            System.out.println(user);
+    });
     }
 }
