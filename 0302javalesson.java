c8869a2bf89341a34c10a1f961fe31b57988bbe9
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-02 16:17:25 +0900

    add

---
+++ 0302javalesson.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String... args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("lemon");
+        
+        System.out.println(fruits.get(0));
+        System.out.println(fruits.get(0));
+    }
+}
