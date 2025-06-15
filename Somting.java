b3babaae28bd0bcedfae5b90ee511d23ff750b92
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-06-15 16:08:25 +0900

    add

--- Somting.java
+++ Somting.java
@@ -1,7 +1,23 @@
 class Sample{
     public static void main(String[] args){
         something();
+        showDiskArea();
+        showDiskArea(10);
+        showDiskArea(10,3141);
     }
+    public static void showDiskArea() {
+        int radius=20;
+        double pi =3.14;
+        System.out.println(radius * radius * pi);
+    }
+    public static void showDiskArea(int radius) {
+        double pi =3.14;
+        System.out.println(radius * radius * pi);
+    }
+    public static void showDiskArea(int radius,double pi) {
+        System.out.println(radius * radius * pi);
+    }
+
     public static void something() {
         System.out.println("Hello");
     }
