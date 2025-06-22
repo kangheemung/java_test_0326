9c8a7281b611b7a558d7707a4296db4614930bea
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-06-22 15:55:27 +0900

    add

---
+++ javaclass_0622.java
@@ -0,0 +1,16 @@
+// class Calculation {
+// // クラスの中身
+// //解答欄の初期値に記載されているプログラムを確認して追記しなさい。
+
+// // 下記のクラスを作成しなさい。作成したExecutionクラス内にmainメソッドを記述し、
+// //Calculationクラスのインスタンスを作成しなさい。
+
+// // また、実行確認を行い、エラーが発生していないことを確認すること。
+
+
+// }
+public class Calculation {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
