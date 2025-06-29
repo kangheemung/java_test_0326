b05f9d59daa54ef8cd80e8a7bdb142f54f936e61
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-06-29 16:01:47 +0900

    test

---
+++ Lesson.java
@@ -0,0 +1,44 @@
+// Personクラスを修正して、コンストラクタを追加する
+class Person {
+    protected String name; // 名前
+
+    // コンストラクタを定義
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Teacher extends Person {
+    private String subject; // 担当教科
+
+    // Teacherクラスのコンストラクタを定義
+    public Teacher(String name, String subject) {
+        super(name); // スーパークラスのコンストラクタを呼び出す
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public void setSubject(String subject) {
+    this.subject = subject;
+    }
+}
+
+class Lesson {
+    public static void main(String[] args) {
+        // コンストラクタを使用してTeacherクラスのインスタンスを生成
+        Teacher teacher = new Teacher("太郎", "数学");
+        System.out.println("名前: " + teacher.getName());
+        System.out.println("科目: " + teacher.getSubject());
+    }
+}
