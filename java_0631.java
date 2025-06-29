9a410fe1783643b0291c9c161542ef636f3f6eef
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-06-29 17:26:19 +0900

    test

--- java_0631.java
+++ java_0631.java
@@ -1,26 +1,30 @@
 class Main{
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.setName("太郎");
+        Person person1 = new Person("太郎");
         person1.setAge(10);
-        Person person2 = new Person();
-        person1.setName("花子");
-        person1.setAge(10);
+
+        Person person2 = new Person("花子1");
+
+        person2.setAge(16);
 
         person1.showFields();
         person2.showFields();
-        Person person_Kang = new Person("kang");
-        person_Kang.setName();
 
+        Person person_Kang = new Person("kang");
+         person_Kang.setAge(35);
+        person_Kang.showFields();
     }
 }
 class Person{
     public String name;
     public int age;
-    public void setName(String name){
+
+    public Person(String name){
         this.name = name;
     }
-
+    public void setName(String name) {
+        this.name = name;
+    }
     public void setAge(int age){
         this.age = age;
     }
