036c858066b1f3420d1829d8d0b8fefb58be6aeb
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-06-29 16:36:34 +0900

    test

--- Lesson.java
+++ Lesson.java
@@ -11,26 +11,23 @@ class Person {
     public String getName() {
         return this.name;
     }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
 }
 
 class Teacher extends Person {
     private String subject; // 担当教科
-
     // Teacherクラスのコンストラクタを定義
     public Teacher(String name, String subject) {
         super(name); // スーパークラスのコンストラクタを呼び出す
         this.subject = subject;
     }
-
     public String getSubject() {
         return this.subject;
     }
     public void setSubject(String subject) {
     this.subject = subject;
+    }
+     public void showMessage(String message) {
+        System.out.println("Teacher says: " + message);
     }
 }
 
@@ -38,6 +35,7 @@ class Lesson {
     public static void main(String[] args) {
         // コンストラクタを使用してTeacherクラスのインスタンスを生成
         Teacher teacher = new Teacher("太郎", "数学");
+        teacher.showMessage("Hello student");
         System.out.println("名前: " + teacher.getName());
         System.out.println("科目: " + teacher.getSubject());
     }
